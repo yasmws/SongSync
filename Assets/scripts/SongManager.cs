410f8ae2ac294cd4d8c0de8eeb28f86251f93f13
Pedro Baptista <pedrolk2012@gmail.com>
2024-02-16 00:49:30 -0300

    added DryWetMidi Library and added logic to read a .mid file from local files

--- Assets/scripts/SongManager.cs
+++ Assets/scripts/SongManager.cs
@@ -1,19 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using System.IO;
 using UnityEngine.Networking;
+using System;
+
 
 public class SongManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SongManager Instance;
+    public AudioSource audioSource;
+    public float songDelayInSeconds;
+    public int inputDelayInMilliSeconds;
+    public double marginOfError;
+
+    public string fileLocation;
+    public float noteTime;
+    public float noteSpawnY;
+    public float noteTapY;
+
+    public float noteDespawnY
     {
-        
+        get
+        {
+            return noteTapY - (noteSpawnY - noteTapY);
+        }
+    }
+    public static MidiFile midiFile;
+
+    private void Start()
+    {
+        Instance = this;
+
+        if(Application.streamingAssetsPath.StartsWith("http://") || Application.streamingAssetsPath.StartsWith("https://"))
+        {
+            ReadFromWebsite();
+        }
+        else
+        {
+            ReadFromFile();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReadFromFile()
     {
-        
+        midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + fileLocation);
+        GetDataFromMidi();
+    }
+
+    private void ReadFromWebsite()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetDataFromMidi()
+    {
+        var notes = midiFile.GetNotes();
+        var array = new Note[notes.Count];
+        notes.CopyTo(array, 0);
+        Invoke(nameof(StartSong), songDelayInSeconds);
+
+    }
+
+    public static double GetAudioSourceTime()
+    {
+        return (double)Instance.audioSource.timeSamples / Instance.audioSource.clip.frequency;
+    }
+
+    public void StartSong()
+    {
+        audioSource.Play();
     }
 }
