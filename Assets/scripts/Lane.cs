1ed95f3af47ea2b87a1c00a01f05ad83555ced76
aliceoq <aliceoqbrito@gmail.com>
2024-02-25 23:11:54 -0300

    show long notes

--- Assets/scripts/Lane.cs
+++ Assets/scripts/Lane.cs
@@ -14,7 +14,7 @@ public class Lane : MonoBehaviour
     public GameObject notePrefab;
     List<Note> notes = new List<Note>();
     public Color laneColor;
-    public List<double> timeStamps = new List<double>();
+    public List<NoteData> timeStamps = new List<NoteData>();
     private SpriteRenderer hitboxRenderer;
 
 
@@ -37,7 +37,9 @@ public class Lane : MonoBehaviour
             if (note.NoteNumber == noteRestriction)
             {
                 var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, SongManager.midiFile.GetTempoMap());
-                timeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
+                var metricTimeSpanDuration = TimeConverter.ConvertTo<MetricTimeSpan>(note.Length, SongManager.midiFile.GetTempoMap());
+                var duration = (double)metricTimeSpanDuration.Minutes * 60f + metricTimeSpanDuration.Seconds + (double)metricTimeSpanDuration.Milliseconds / 1000f;
+                timeStamps.Add(new NoteData((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f, duration));
             }
         }
     }
@@ -59,19 +61,40 @@ public class Lane : MonoBehaviour
         if (spawnIndex < timeStamps.Count)
         {
 
-            if (SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - SongManager.Instance.noteTime)
+            if (SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex].timestamp - SongManager.Instance.noteTime)
             {
                 var note = Instantiate(notePrefab, transform);
                 notes.Add(note.GetComponent<Note>());
-                note.GetComponent<Note>().assignedTime = (float)timeStamps[spawnIndex];
+                note.GetComponent<Note>().assignedTime = (float)timeStamps[spawnIndex].timestamp;
+                note.GetComponent<Note>().duration = (float)timeStamps[spawnIndex].duration;
+                Vector3 notePosition = note.transform.localPosition;
+                notePosition.y = SongManager.Instance.noteSpawnY;
+                note.transform.localPosition = notePosition;
+
+                var childTransform = note.transform.GetChild(0);
+                Vector3 scale = childTransform.localScale;
+                scale.y = ((float) timeStamps[spawnIndex].duration * SongManager.Instance.noteDistanceToTap) / SongManager.Instance.noteTime;
+                childTransform.localScale = scale;
+
+                float halfHeight = scale.y / 2.0f;
+                Vector3 newPosition = childTransform.localPosition;
+                newPosition.y = halfHeight;
+                childTransform.localPosition = newPosition;
+
                 note.GetComponent<SpriteRenderer>().color = laneColor;
-               spawnIndex++;
+                childTransform.GetComponent<SpriteRenderer>().color = laneColor;
+
+                //Debug.Log("on spawn: " + note.transform.localPosition);
+                note.GetComponent<SpriteRenderer>().enabled = true;
+                childTransform.GetComponent<SpriteRenderer>().enabled = true;
+
+                spawnIndex++;
             }
         }
 
         if (inputIndex < timeStamps.Count)
         {
-            double timeStamp = timeStamps[inputIndex];
+            double timeStamp = timeStamps[inputIndex].timestamp;
             double marginOfError = SongManager.Instance.marginOfError;
             double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.inputDelayInMilliseconds / 1000.0);
 
