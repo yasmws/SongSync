fa79bac9d3966a3aeed572639d707138db086e63
victoria <vlsc@cin.ufpe.br>
2024-03-04 23:24:00 -0300

    feat(SongSelect): select song working

--- Assets/scripts/SongManager.cs
+++ Assets/scripts/SongManager.cs
@@ -6,11 +6,13 @@ using Melanchall.DryWetMidi.Interaction;
 using System.IO;
 using UnityEngine.Networking;
 using System;
+using UnityEditor;
 
 public class SongManager : MonoBehaviour
 {
     public static SongManager Instance;
     public AudioSource audioSource;
+    public AudioSource[] audioSources = new AudioSource[3];
     public Lane[] lanes;
     public float songDelayInSeconds;
     public int inputDelayInMilliseconds;
@@ -19,6 +21,7 @@ public class SongManager : MonoBehaviour
     public ICollection<Melanchall.DryWetMidi.Interaction.Note> notes;
     public float minLongNoteDuration;
 
+    public int played = 0;
 
     public string fileLocation;
     public float noteTime;
@@ -82,8 +85,61 @@ public class SongManager : MonoBehaviour
         fileLocation = "Assets/Audio/" + songFolder +"/notes.mid";
         midiFile = MidiFile.Read(fileLocation);
         Debug.Log(fileLocation);
+
+        Import(songFolder);
         GetDataFromMidi();
     }
+
+    void Import(string songFolder)
+    {
+        #if UNITY_EDITOR
+        string guitarPath = "Assets/Audio/" + songFolder +"/guitar.ogg";
+        string songPath = "Assets/Audio/" + songFolder +"/song.ogg";
+        string rhythmPath = "Assets/Audio/" + songFolder +"/rhythm.ogg";
+
+        if (!File.Exists(guitarPath))
+        {
+            Debug.LogError("File not found: " + guitarPath);
+            return;
+        }
+         if (!File.Exists(songPath))
+        {
+            Debug.LogError("File not found: " + songPath);
+            return;
+        }
+         if (!File.Exists(rhythmPath))
+        {
+            Debug.LogError("File not found: " + rhythmPath);
+            return;
+        }
+
+        AssetDatabase.ImportAsset(guitarPath, ImportAssetOptions.ForceUpdate);
+        AssetDatabase.ImportAsset(songPath, ImportAssetOptions.ForceUpdate);
+        AssetDatabase.ImportAsset(rhythmPath, ImportAssetOptions.ForceUpdate);
+
+        AudioClip songClip = AssetDatabase.LoadAssetAtPath<AudioClip>(guitarPath);
+        AudioClip songClip2 = AssetDatabase.LoadAssetAtPath<AudioClip>(songPath);
+        AudioClip songClip3 = AssetDatabase.LoadAssetAtPath<AudioClip>(rhythmPath);
+
+        if (songClip != null)
+        {
+            Debug.Log("Song imported successfully: " + songClip.name);
+
+            Instance.audioSources[0].clip = songClip;
+            Instance.audioSources[1].clip = songClip2;
+            Instance.audioSources[2].clip = songClip3;
+
+            Instance.audioSource.clip = songClip;
+        }
+        else
+        {
+            Debug.LogError("Failed to import song");
+        }
+        #else
+        Debug.LogError("This script can only be used in the Unity Editor");
+        #endif
+    }
+
     public void GetDataFromMidi()
     {
         notes = midiFile.GetNotes();
@@ -94,7 +150,13 @@ public class SongManager : MonoBehaviour
     }
     public void StartSong()
     {
-        audioSource.Play();
+        Instance.audioSource.Play();
+
+        Instance.audioSources[0].Play();
+        Instance.audioSources[1].Play();
+        Instance.audioSources[2].Play();
+
+        played = 1;
     }
     public static double GetAudioSourceTime()
     {
@@ -103,9 +165,10 @@ public class SongManager : MonoBehaviour
 
     void Update()
     {
-        if (!audioSource.isPlaying)
+        if (Instance.audioSource.clip.name == "guitar" && !Instance.audioSource.isPlaying && played == 1)
         {
             ScreenManager.Instance.changeScene("EndGameScene");
+            played = 0;
         }
     }
 }
